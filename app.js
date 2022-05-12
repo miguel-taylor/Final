4c3fab6c11f1102e4bdd0480aa82a4df1c9bda0d
Miguel Taylor <migueltaylor.tx@gmail.com>
2022-05-12 16:26:07 -0500

    finished

--- app.js
+++ app.js
@@ -41,4 +41,8 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         todo.remove ()
     }
+    if(item.classList[0] === "complete-btn" ) {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
